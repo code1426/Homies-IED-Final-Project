77918b071c3fd6893b0a9470f7464cadd26bc129
Rops-11 <jrofer.casio11@gmail.com>
2024-03-24 22:01:18 +0800

    Making of Message Component, and Updating the Message Screen

---
+++ app/components/Messages.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Image,
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+
+// name is just a string of a name of the person
+// profilePic property must have a req('img') as input
+// latest_message is a string remove the {} and just use quotations
+// time is the time the last message has been sent remove the {} and just use quotations
+
+function Messages({ name, profilePic, latestMessage, time }) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.pressableContent}>
+        <Image
+          source={profilePic}
+          style={styles.profilePicStyle}
+        />
+        <View style={[styles.messageContent]}>
+          <Text style={styles.profileName}>{name}</Text>
+          <Text style={styles.message}>{latestMessage}</Text>
+        </View>
+        <View style={[styles.updateSeen]}>
+          <Text style={styles.timeUpdated}>{time}</Text>
+          <Image
+            source={profilePic}
+            style={styles.messageSeenProfileStyle}
+          />
+        </View>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 'auto',
+    alignItems: 'stretch',
+    // flex: 1,
+    flexDirection: 'column',
+  },
+  pressableContent: {
+    height: 79,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    // justifyContent: 'space-evenly',
+  },
+  profilePicStyle: {
+    alignSelf: 'center',
+    width: 55,
+    height: 55,
+    borderRadius: 27.5,
+    marginLeft: 35,
+    resizeMode: 'contain',
+  },
+  messageContent: {
+    width: 227,
+    left: 16,
+    marginTop: 15,
+  },
+  profileName: {
+    fontSize: 16.5,
+    fontWeight: 'bold',
+  },
+  message: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  updateSeen: {
+    direction: 'rtl',
+    alignSelf: 'center',
+    right: 21,
+    position: 'absolute',
+  },
+  timeUpdated: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    marginBottom: 6,
+  },
+  messageSeenProfileStyle: {
+    // alignSelf: 'center',
+    width: 21,
+    height: 21,
+    borderRadius: 10.5,
+    resizeMode: 'contain',
+  },
+});
+
+export default Messages;
--- app/screens/MessagesScreen.jsx
+++ app/screens/MessagesScreen.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import Messages from '../components/Messages';
+import { Text, SafeAreaView, Image, StyleSheet } from 'react-native';
 
 function MessagesScreen(props) {
   return (
-    <SafeAreaView>
-      <Text>Messages Screen</Text>
+    <SafeAreaView style={styles.screen}>
+      <Text style={styles.title}>Messages</Text>
+      <Messages
+        profilePic={require('../assets/icon.png')}
+        name='Yay Me Yao'
+        latestMessage='Hello, how are you?'
+        time = '8:15 PM'
+      />
+      <Messages
+        profilePic={require('../assets/icon.png')}
+        name='Wansapanataym'
+        latestMessage='Wassup bro'
+        time = '8:15 AM'
+      />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 21,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    marginBottom: 45,
+    marginTop: 15
+  },
+  screen: {
+    flex: 1,
+    backgroundColor: '#ECEFF6',
+  },
+});
 
 export default MessagesScreen;
