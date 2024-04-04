c46928cb99871c2c1bc4d952ac2c3b16cd3bf3f2
Vergara0826 <kimlyjohn.vergara-23@cpu.edu.ph>
2024-04-04 16:34:36 +0800

    Update AddPropertyScreen.jsx

--- app/screens/AddPropertyScreen.jsx
+++ app/screens/AddPropertyScreen.jsx
@@ -33,15 +33,19 @@ function AddPropertyScreen ({ navigation }) {
 
   const [propertyList, setPropertyList] = useState([]);
 
-  const [image, setImage] = useState(null);
+  const [thumbnail, setThumbnail] = useState(null);
+  const [images, setImages] = useState({image1: null, image2: null, image3: null})
 
   const [titleStyle, setTitleStyle] = useState({...styles.titleInput})
   const [descStyle, setDescStyle] = useState({...styles.descriptionInput})
   const [propertyStyle, setPropertyStyle] = useState({...styles.pickerContainer})
   const [rentStyle, setRentStyle] = useState({...styles.rentInput})
   const [regStyle, setRegStyle] = useState({...styles.regInput})
-  const [imgStyle, setImgStyle] = useState({...styles.image})
+  const [imgStyle, setImgStyle] = useState({...styles.thumbnail })
   const [featureStyle, setFeatureStyle] = useState({...styles.featuresContainer})
+  const [image1Style, setImage1Style] = useState({...styles.image1})
+  const [image2Style, setImage2Style] = useState({...styles.image2})
+  const [image3Style, setImage3Style] = useState({...styles.image3})
 
   const [loading, setLoading] = useState(false)
   const [isSubmitted, setIsSubmitted] = useState(false)
@@ -66,7 +70,7 @@ function AddPropertyScreen ({ navigation }) {
     })
   }
 
-  const pickImage = async () => {
+  const pickThumbnail = async () => {
     // No permissions request is necessary for launching the image library
     try {
 let result = await ImagePicker.launchImageLibraryAsync({
@@ -80,7 +84,74 @@ let result = await ImagePicker.launchImageLibraryAsync({
   
       if (!result.canceled) {
         // console.log(image);
-        setImage(result.assets[0].uri);
+        // console.log('THUMBNAIL => ', result.assets[0].uri)
+        setThumbnail(result.assets[0].uri);
+        // console.log(image);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const pickImage1 = async () => {
+    // No permissions request is necessary for launching the image library
+    try {
+let result = await ImagePicker.launchImageLibraryAsync({
+  mediaTypes: ImagePicker.MediaTypeOptions.All,
+  allowsEditing: true,
+  aspect: [4, 4],
+  quality: 1,
+      });
+  
+      // console.log(result);
+  
+      if (!result.canceled) {
+        // console.log(image);
+        setImages({ ...images, image1:result.assets[0].uri });
+        // console.log(image);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const pickImage2 = async () => {
+    // No permissions request is necessary for launching the image library
+    try {
+let result = await ImagePicker.launchImageLibraryAsync({
+  mediaTypes: ImagePicker.MediaTypeOptions.All,
+  allowsEditing: true,
+  aspect: [4, 4],
+  quality: 1,
+      });
+  
+      // console.log(result);
+  
+      if (!result.canceled) {
+        // console.log(image);
+        setImages({ ...images, image2: result.assets[0].uri });
+        // console.log(image);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const pickImage3 = async () => {
+    // No permissions request is necessary for launching the image library
+    try {
+let result = await ImagePicker.launchImageLibraryAsync({
+  mediaTypes: ImagePicker.MediaTypeOptions.All,
+  allowsEditing: true,
+  aspect: [4, 4],
+  quality: 1,
+      });
+  
+      // console.log(result);
+  
+      if (!result.canceled) {
+        // console.log(image);
+        setImages({ ...images, image3:result.assets[0].uri });
         // console.log(image);
       }
     } catch (error) {
@@ -95,70 +166,123 @@ let result = await ImagePicker.launchImageLibraryAsync({
     // console.log(selected)
 
     for (let [key, val] of Object.entries(values)) {
-      if (!val && key !== 'image') {
+      if (!val && key !== 'thumbnail' && key !== 'images') {
         isAllInputFilled = false;
         console.log(`Error at: ${key}`)
-        ToastAndroid.show('All input is required', ToastAndroid.SHORT)
+        // ToastAndroid.show('All input is required', ToastAndroid.SHORT)
         return
       } else isAllInputFilled = true;
     }
 
     if (isNaN(values.rentPrice)) {
       isAllInputFilled = false
-      ToastAndroid.show('Please enter a number for the rent price.', ToastAndroid.SHORT)
+      // ToastAndroid.show('Please enter a number for the rent price.', ToastAndroid.SHORT)
       setRentStyle({ ...styles.rentInput, borderWidth: 2, borderColor: '#FF2525' })
       return
     }
     if (isNaN(values.registrationPrice)) {
       isAllInputFilled = false
-      ToastAndroid.show('Please enter a number for the registration.', ToastAndroid.SHORT)
+      // ToastAndroid.show('Please enter a number for the registration.', ToastAndroid.SHORT)
       setRegStyle({ ...styles.regInput, borderWidth: 2, borderColor: '#FF2525' })
       return
     }
     if (parseInt(values.rentPrice) < parseInt(values.registrationPrice)) {
       isAllInputFilled = false
-      ToastAndroid.show('Down payment must be less than or equal to rent price:', ToastAndroid.SHORT)
+      // ToastAndroid.show('Down payment must be less than or equal to rent price:', ToastAndroid.SHORT)
       setRegStyle({ ...styles.regInput, borderWidth: 2, borderColor: '#FF2525' })
       return
     }
 
     try {
       setLoading(true)
-      const response = await fetch(image)
-      const blob = await response.blob()
-      const storageRef = ref(storage, 'ownerPosts/'+Date.now()+'.jpg');
+      const thumbnailRes = await fetch(thumbnail)
+      const image1 = await fetch(images.image1)
+      const image2 = await fetch(images.image2)
+      const image3 = await fetch(images.image3)
 
-      uploadBytes(storageRef, blob).then((snapshot) => {
+      const thumbnailBlob = await thumbnailRes.blob()
+      const image1Blob = await image1.blob()
+      const image2Blob = await image2.blob()
+      const image3Blob = await image3.blob()
+
+      const storageRef = ref(storage, 'ownerPosts/thumbnail/'+Date.now()+'.jpg');
+      const image1Ref = ref(storage, 'ownerPosts/image1/'+Date.now()+'.jpg');
+      const image2Ref = ref(storage, 'ownerPosts/image2/'+Date.now()+'.jpg');
+      const image3Ref = ref(storage, 'ownerPosts/image3/'+Date.now()+'.jpg');
+
+      uploadBytes(image3Ref, image3Blob).then((snapshot) => {
+        console.log('Uploaded a blob or file! 3333');
+      }).then((resp)=> {
+        getDownloadURL(image3Ref).then( async (downloadUrl) => {
+          values.images.image3 = downloadUrl
+          console.log('image3 set successfully');
+        })
+      })
+
+      uploadBytes(storageRef, thumbnailBlob).then((snapshot) => {
         console.log('Uploaded a blob or file!');
       }).then((resp) => {
         getDownloadURL(storageRef)
         .then( async (downloadUrl) => {
           // console.log(downloadUrl);
-          values.image = downloadUrl
-          console.log('Image set successfully', resp)
+          values.thumbnail = downloadUrl
+          console.log('thumbnail set successfully')
       
-          if (isAllInputFilled) {
-            // add the data to the firestore
-            const docRef = await addDoc(collection(FirebaseDB, 'OwnerPosts'), values)
-              
-            if (docRef.id) {
-              setLoading(false);
-              console.log('document added!')
-              Alert.alert('Success!', 'Post Added Successfully.', [{text: 'OK', onPress: () => {
-                navigation.navigate('Home')
-                setImage(null)
-                setImgStyle({...styles.image})
-                Method.resetForm({values: formInitialValues})
-              }
-            }], {cancelable: false})
-            }
-          }
+          uploadBytes(image1Ref, image1Blob).then((snapshot) => {
+            console.log('Uploaded a blob or file! 1111');
+          }).then((resp)=> {
+            getDownloadURL(image1Ref).then( async (downloadUrl) => {
+              values.images.image1 = downloadUrl
+              console.log('image1 set successfully');
+    
+              uploadBytes(image2Ref, image2Blob).then((snapshot) => {
+                console.log('Uploaded a blob or file! 2222');
+              }).then((resp)=> {
+                getDownloadURL(image2Ref).then( async (downloadUrl) => {
+                  values.images.image2 = downloadUrl
+                  console.log('image2 set successfully');
+    
+                  uploadBytes(image2Ref, image2Blob).then((snapshot) => {
+                    console.log('Uploaded a blob or file! 2222');
+                  }).then((resp)=> {
+                    getDownloadURL(image2Ref).then( async (downloadUrl) => {
+                      values.images.image2 = downloadUrl
+                      console.log('image2 set successfully');
+    
+                      if (isAllInputFilled) {
+                        // add the data to the firestore
+                        const docRef = await addDoc(collection(FirebaseDB, 'OwnerPosts'), values)
+                          
+                        if (docRef.id) {
+                          setLoading(false);
+                          console.log('document added!')
+                          Alert.alert('Success!', 'Post Added Successfully.', [{text: 'OK', onPress: () => {
+                            navigation.navigate('Home')
+                            setThumbnail(null)
+                            setImages({image1: null, image2: null, image3: null})
+                            setImgStyle({...styles.thumbnail})
+                            setImage1Style({...styles.image1})
+                            setImage2Style({...styles.image2})
+                            setImage3Style({...styles.image3})
+                            Method.resetForm({values: formInitialValues})
+                          }
+                        }], {cancelable: false})
+                        }
+                      }
+                    })
+                  })
+                })
+              })
+            })
+          })
         })
       })
+
+
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message, 'main')
       setLoading(false)
-      ToastAndroid.show('Image is Required', ToastAndroid.SHORT)
+      // ToastAndroid.show('Thumbnail is Required', ToastAndroid.SHORT)
     }
   }
 
@@ -188,11 +312,11 @@ let result = await ImagePicker.launchImageLibraryAsync({
       setRegStyle({ ...styles.regInput, borderWidth: 2, borderColor: '#FF2525' })
     } else {
       setRegStyle({...styles.regInput})
-    } if (!values.image) {
-      setImgStyle({ ...styles.image, borderWidth: 2, borderColor: '#FF2525' })
-      console.log('Image is required')
+    } if (!values.thumbnail) {
+      setImgStyle({ ...styles.thumbnail, borderWidth: 2, borderColor: '#FF2525' })
+      console.log('thumbnail is required')
     } else {
-      setImgStyle({...styles.image})
+      setImgStyle({...styles.thumbnail})
     } if (isNaN(values.rentPrice)) {
       setRentStyle({ ...styles.rentInput, borderWidth: 2, borderColor: '#FF2525' })
     }
@@ -200,7 +324,21 @@ let result = await ImagePicker.launchImageLibraryAsync({
       setRegStyle({ ...styles.regInput, borderWidth: 2, borderColor: '#FF2525' })
     } if (!values.features[0]) {
       setFeatureStyle({ ...styles.featuresContainer, borderWidth: 2, borderColor: '#FF2525' })
-    } else setFeatureStyle({ ...styles.featuresContainer})
+    } else {
+      setFeatureStyle({ ...styles.featuresContainer})
+    } if (!values.images.image1) {
+      setImage1Style({ ...styles.image1, borderWidth: 2, borderColor: '#FF2525' })
+    } else {
+      setImage1Style({...styles.image1})
+    } if (!values.images.image2) {
+      setImage2Style({ ...styles.image2, borderWidth: 2, borderColor: '#FF2525' })
+    } else {
+      setImage2Style({...styles.image2})
+    } if (!values.images.image3) {
+      setImage3Style({ ...styles.image3, borderWidth: 2, borderColor: '#FF2525' })
+    } else {
+      setImage3Style({...styles.image3})
+    }
   } 
 
   const formInitialValues = {
@@ -209,7 +347,12 @@ let result = await ImagePicker.launchImageLibraryAsync({
     rentPrice:"", 
     propertyType:null, 
     registrationPrice:"",
-    image:null,
+    thumbnail:null,
+    images: {
+      image1: null, 
+      image2: null, 
+      image3: null
+    },
     features:[]
   }
 
@@ -219,16 +362,6 @@ let result = await ImagePicker.launchImageLibraryAsync({
     { label: 'Shared Room', value: 'Shared Room'},
     { label: 'Air Conditioned', value: 'Air Conditioned' },
   ];
-
-  const [selected, setSelected] = useState([]);
-
-  const renderItem = item => {
-    return (
-      <View style={{borderRadius: 20}}>
-        <Text style={{backgroundColor: 'red'}}>{item.label}</Text>
-      </View>
-    );
-  };
 
   return (
     <SafeAreaView>
@@ -261,10 +394,10 @@ let result = await ImagePicker.launchImageLibraryAsync({
               <View style={styles.container}>
 
                 {/* set image */}
-                <TouchableOpacity onPress={pickImage}
-                  style={styles.imageContainer}
+                <TouchableOpacity onPress={pickThumbnail}
+                  style={styles.thumbnailContainer}
                 >
-                  {image ? <Image source={{ uri:image }} style={styles.image}/> 
+                  {thumbnail ? <Image source={{ uri:thumbnail }} style={styles.thumbnail}/> 
                   :
                   <>
                   <Image source={require('../assets/placeholder.png')}
@@ -275,6 +408,55 @@ let result = await ImagePicker.launchImageLibraryAsync({
                   </View>
                   </>}
                 </TouchableOpacity>
+
+                <View style={{flexDirection: 'row', flex:1, width: 330, justifyContent: 'space-between', alignSelf: 'center' }}>
+
+                  <TouchableOpacity onPress={pickImage1}
+                    style={styles.imageContainer}
+                  >
+                    {images.image1 ? <Image source={{ uri:images.image1 }} style={styles.image1}/>
+                    :
+                    <>
+                    <Image source={require('../assets/placeholder.png')}
+                    style={image1Style}
+                    />
+                    <View style={styles.imageAddIconContainer}> 
+                      <Image resizeMode='cover' style={styles.addIcon} source={require('../assets/add.png')}/>
+                    </View>
+                    </>}
+                  </TouchableOpacity>
+
+                  <TouchableOpacity onPress={pickImage2}
+                    style={styles.imageContainer}
+                  >
+                    {images.image2 ? <Image source={{ uri:images.image2 }} style={styles.image2}/>
+                    :
+                    <>
+                    <Image source={require('../assets/placeholder.png')}
+                    style={image2Style}
+                    />
+                    <View style={styles.imageAddIconContainer}> 
+                      <Image resizeMode='cover' style={styles.addIcon} source={require('../assets/add.png')}/>
+                    </View>
+                    </>}
+                  </TouchableOpacity>
+
+                  <TouchableOpacity onPress={pickImage3}
+                    style={styles.imageContainer}
+                  >
+                    {images.image3 ? <Image source={{ uri:images.image3 }} style={styles.image3}/>
+                    :
+                    <>
+                    <Image source={require('../assets/placeholder.png')}
+                    style={image3Style}
+                    />
+                    <View style={styles.imageAddIconContainer}> 
+                      <Image resizeMode='cover' style={styles.addIcon} source={require('../assets/add.png')}/>
+                    </View>
+                    </>}
+                  </TouchableOpacity>
+
+                </View>
 
                 {/* title container */}
                 <View>
@@ -399,6 +581,7 @@ export default AddPropertyScreen;
 
 const styles = StyleSheet.create({
   container: {
+    // backgroundColor: 'red',
     padding: 30,
     paddingBottom: 150,
     height: '100%'
@@ -481,7 +664,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     padding: 8,
   },
-  imageContainer: {
+  thumbnailContainer: {
     postion: 'relative',
     justifyContent: 'center',
     alignSelf: 'center',
@@ -489,7 +672,7 @@ const styles = StyleSheet.create({
     height: 330,
     marginBottom: 20,
   },
-  image: {
+  thumbnail: {
     flex: 1,
     width: '100%',
     height: '100%',
@@ -507,5 +690,43 @@ const styles = StyleSheet.create({
   addIcon: {
     width: '100%',
     height: '100%'
-  }
+  },
+  imageContainer: {
+    postion: 'relative',
+    justifyContent: 'center',
+    // alignSelf: 'center',
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  image1: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    resizeMode: 'contain',
+  },
+  image2: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    resizeMode: 'contain',
+  },
+  image3: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    resizeMode: 'contain',
+  },
+  imageAddIconContainer: {
+    postion: 'absolute',
+    width: 20,
+    height: 20,
+    top: -60,
+    alignSelf: 'center',
+    marginBottom: -20
+  },
 })
