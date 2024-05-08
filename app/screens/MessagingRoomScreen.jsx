36453d55854ff2b8567cec078c9ca99e570e2015
Vergara0826 <kimlyjohn.vergara-23@cpu.edu.ph>
2024-05-08 10:53:24 +0800

    Update MessagingRoomScreen.jsx

--- app/screens/MessagingRoomScreen.jsx
+++ app/screens/MessagingRoomScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 import {
   SafeAreaView,
   View,
@@ -13,11 +13,11 @@ import {
   Platform,
   Text,
   ActivityIndicator,
-} from 'react-native';
+} from "react-native";
 // import HeaderComponent from "../components/HeaderComponent";
-import MessagingRoomHeaderComponent from '../components/MessagingRoomHeaderComponent';
-import { MessageContext, UserContext } from '../../Contexts';
-import Message from '../components/MessagingComponents/Message';
+import MessagingRoomHeaderComponent from "../components/MessagingRoomHeaderComponent";
+import { MessageContext, UserContext } from "../../Contexts";
+import Message from "../components/MessagingComponents/Message";
 import {
   Timestamp,
   arrayUnion,
@@ -25,11 +25,11 @@ import {
   onSnapshot,
   serverTimestamp,
   updateDoc,
-} from 'firebase/firestore';
-import { FirebaseDB } from '../../firebase.config';
-import { useRef } from 'react';
-import { KeyboardAwareFlatList } from 'react-native-keyboard-aware-scroll-view';
-import { useNavigation } from '@react-navigation/native';
+} from "firebase/firestore";
+import { FirebaseDB } from "../../firebase.config";
+import { useRef } from "react";
+import { KeyboardAwareFlatList } from "react-native-keyboard-aware-scroll-view";
+import { useNavigation } from "@react-navigation/native";
 
 function MessagingRoomScreen({ navigation }) {
   const currentUser = useContext(UserContext);
@@ -48,7 +48,7 @@ function MessagingRoomScreen({ navigation }) {
   useEffect(() => {
     try {
       const unSub = onSnapshot(
-        doc(FirebaseDB, 'Messages', data.MessageId),
+        doc(FirebaseDB, "Messages", data.MessageId),
         (doc) => {
           doc.exists() && setMessages(doc.data().messages.reverse());
         }
@@ -64,7 +64,7 @@ function MessagingRoomScreen({ navigation }) {
   useEffect(() => {
     navigation.getParent()?.setOptions({
       tabBarStyle: {
-        display: 'none',
+        display: "none",
         bottom: 0,
       },
     });
@@ -75,16 +75,16 @@ function MessagingRoomScreen({ navigation }) {
   }, [navigation]);
 
   // Sending Message
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [reserveFee, setReserveFee] = useState();
   const [popUpFunctions, setPopUpFunctions] = useState(false);
   const [reservePopUp, setReservePopUp] = useState(false);
 
   const handleReserve = async () => {
     try {
-      setReserveFee('');
-      if (reserveFee !== '') {
-        await updateDoc(doc(FirebaseDB, 'Messages', data.MessageId), {
+      setReserveFee("");
+      if (reserveFee !== "") {
+        await updateDoc(doc(FirebaseDB, "Messages", data.MessageId), {
           messages: arrayUnion({
             id: Math.random().toString(16).slice(2),
             reserveFee: parseInt(reserveFee) + parseInt(reserveFee) * 0.01,
@@ -94,26 +94,30 @@ function MessagingRoomScreen({ navigation }) {
           }),
         });
 
-        await updateDoc(doc(FirebaseDB, 'UserMessages', currentUser.uid), {
-          [data.MessageId + '.latestMessage']: {
-            text: `Reservation Offer: Php ${(parseFloat(reserveFee) * 1.01).toLocaleString()}`,
+        await updateDoc(doc(FirebaseDB, "UserMessages", currentUser.uid), {
+          [data.MessageId + ".latestMessage"]: {
+            text: `Reservation Offer: Php ${(
+              parseFloat(reserveFee) * 1.01
+            ).toLocaleString()}`,
           },
-          [data.MessageId + '.date']: Timestamp.now()
+          [data.MessageId + ".date"]: Timestamp.now()
             .toDate()
             .toString()
-            .split(' ')[4]
-            .split(':'),
+            .split(" ")[4]
+            .split(":"),
         });
 
-        await updateDoc(doc(FirebaseDB, 'UserMessages', data.user.uid), {
-          [data.MessageId + '.latestMessage']: {
-            text: `Reservation Offer: Php ${(parseFloat(reserveFee) * 1.01).toLocaleString()}`,
+        await updateDoc(doc(FirebaseDB, "UserMessages", data.user.uid), {
+          [data.MessageId + ".latestMessage"]: {
+            text: `Reservation Offer: Php ${(
+              parseFloat(reserveFee) * 1.01
+            ).toLocaleString()}`,
           },
-          [data.MessageId + '.date']: Timestamp.now()
+          [data.MessageId + ".date"]: Timestamp.now()
             .toDate()
             .toString()
-            .split(' ')[4]
-            .split(':'),
+            .split(" ")[4]
+            .split(":"),
         });
       }
       console.log(data);
@@ -124,9 +128,9 @@ function MessagingRoomScreen({ navigation }) {
 
   const handleSend = async () => {
     try {
-      setText('');
-      if (text !== '') {
-        await updateDoc(doc(FirebaseDB, 'Messages', data.MessageId), {
+      setText("");
+      if (text !== "") {
+        await updateDoc(doc(FirebaseDB, "Messages", data.MessageId), {
           messages: arrayUnion({
             id: Math.random().toString(16).slice(2),
             text,
@@ -136,26 +140,26 @@ function MessagingRoomScreen({ navigation }) {
           }),
         });
 
-        await updateDoc(doc(FirebaseDB, 'UserMessages', currentUser.uid), {
-          [data.MessageId + '.latestMessage']: {
+        await updateDoc(doc(FirebaseDB, "UserMessages", currentUser.uid), {
+          [data.MessageId + ".latestMessage"]: {
             text,
           },
-          [data.MessageId + '.date']: Timestamp.now()
+          [data.MessageId + ".date"]: Timestamp.now()
             .toDate()
             .toString()
-            .split(' ')[4]
-            .split(':'),
+            .split(" ")[4]
+            .split(":"),
         });
 
-        await updateDoc(doc(FirebaseDB, 'UserMessages', data.user.uid), {
-          [data.MessageId + '.latestMessage']: {
+        await updateDoc(doc(FirebaseDB, "UserMessages", data.user.uid), {
+          [data.MessageId + ".latestMessage"]: {
             text,
           },
-          [data.MessageId + '.date']: Timestamp.now()
+          [data.MessageId + ".date"]: Timestamp.now()
             .toDate()
             .toString()
-            .split(' ')[4]
-            .split(':'),
+            .split(" ")[4]
+            .split(":"),
         });
       }
       console.log(data);
@@ -167,15 +171,16 @@ function MessagingRoomScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView
-        keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : -55}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={styles.container}>
+        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : -55}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
         <MessagingRoomHeaderComponent
-          title={data.user.firstName + ' ' + data.user.lastName}
+          title={data.user.firstName + " " + data.user.lastName}
           profilePic={{ uri: data.user.photoURL }}
         />
         <FlatList
-          keyboardDismissMode='interactive'
+          keyboardDismissMode="interactive"
           style={styles.room}
           data={messages}
           renderItem={({ item }) => <Message message={item} />}
@@ -186,10 +191,11 @@ function MessagingRoomScreen({ navigation }) {
             style={styles.functionButtons}
             onPress={() => {
               setPopUpFunctions(true);
-            }}>
+            }}
+          >
             <Image
               style={styles.functionButtonsImage}
-              source={require('../assets/three-dots.png')}
+              source={require("../assets/three-dots.png")}
             />
           </TouchableOpacity>
           <TextInput
@@ -202,7 +208,7 @@ function MessagingRoomScreen({ navigation }) {
           />
           <TouchableOpacity onPress={handleSend}>
             <Image
-              source={require('../assets/send-message.png')}
+              source={require("../assets/send-message.png")}
               style={styles.sendButton}
             />
           </TouchableOpacity>
@@ -211,20 +217,22 @@ function MessagingRoomScreen({ navigation }) {
           <View style={styles.popUpContainer}>
             <View style={styles.popUp}>
               <View style={{ paddingHorizontal: 20, paddingVertical: 20 }}>
-                <Text style={styles.popUpTitle}>    Reservation    </Text>
+                <Text style={styles.popUpTitle}> Reservation </Text>
               </View>
               <View>
                 <View style={styles.popUpButtons}>
                   <TouchableOpacity
-                    style={[styles.popUpButton, { backgroundColor: '#5edf95' }]}
+                    style={[styles.popUpButton, { backgroundColor: "#5edf95" }]}
                     onPress={() => {
                       setReservePopUp(true);
                       setPopUpFunctions(false);
-                    }}>
+                    }}
+                  >
                     <Text
                       style={{
-                        textAlign: 'center',
-                      }}>
+                        textAlign: "center",
+                      }}
+                    >
                       Offer Reservation
                     </Text>
                   </TouchableOpacity>
@@ -232,11 +240,13 @@ function MessagingRoomScreen({ navigation }) {
                     style={styles.popUpButton}
                     onPress={() => {
                       setPopUpFunctions(false);
-                    }}>
+                    }}
+                  >
                     <Text
                       style={{
-                        textAlign: 'center',
-                      }}>
+                        textAlign: "center",
+                      }}
+                    >
                       Cancel
                     </Text>
                   </TouchableOpacity>
@@ -248,20 +258,26 @@ function MessagingRoomScreen({ navigation }) {
         {reservePopUp && (
           <View style={styles.popUpContainer}>
             {loading ? (
-              <ActivityIndicator
-                size='small'
-                color='white'
-              />
+              <ActivityIndicator size="small" color="white" />
             ) : (
               <View style={styles.popUp}>
                 <View style={{ paddingHorizontal: 20, paddingVertical: 20 }}>
                   <Text style={styles.popUpTitle}>Reservation Offer</Text>
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      color: "#505050",
+                      marginTop: 8,
+                    }}
+                  >
+                    We will add a 1% service fee.
+                  </Text>
                   <TextInput
-                    placeholder='How much?'
+                    placeholder="How much?"
                     style={styles.reserveFee}
                     onChangeText={setReserveFee}
                     value={reserveFee}
-                    keyboardType='numeric'
+                    keyboardType="numeric"
                   />
                 </View>
                 <View>
@@ -270,11 +286,13 @@ function MessagingRoomScreen({ navigation }) {
                       style={styles.popUpReserveButton}
                       onPress={() => {
                         setReservePopUp(false);
-                      }}>
+                      }}
+                    >
                       <Text
                         style={{
-                          textAlign: 'center',
-                        }}>
+                          textAlign: "center",
+                        }}
+                      >
                         Cancel
                       </Text>
                     </TouchableOpacity>
@@ -282,7 +300,8 @@ function MessagingRoomScreen({ navigation }) {
                       style={[
                         styles.popUpReserveButton,
                         {
-                          backgroundColor: '#5edf95',
+                          backgroundColor: "#5edf95",
+                          borderLeftWidth: 0.6,
                           borderBottomRightRadius: 15,
                         },
                       ]}
@@ -291,11 +310,14 @@ function MessagingRoomScreen({ navigation }) {
                         await handleReserve();
                         setReservePopUp(false);
                         setLoading(false);
-                      }}>
+                      }}
+                    >
                       <Text
                         style={{
-                          textAlign: 'center',
-                        }}>
+                          textAlign: "center",
+                          flex: 1,
+                        }}
+                      >
                         Offer
                       </Text>
                     </TouchableOpacity>
@@ -312,8 +334,8 @@ function MessagingRoomScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5F5F5',
-    flexDirection: 'column',
+    backgroundColor: "#F5F5F5",
+    flexDirection: "column",
   },
   room: {
     // backgroundColor: "white",
@@ -322,16 +344,16 @@ const styles = StyleSheet.create({
     // marginLeft: 30,
     // flex: 1,
     paddingHorizontal: 12,
-    flexDirection: 'row',
+    flexDirection: "row",
     // backgroundColor: "#E0E0E0",
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     height: 72,
   },
   input: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlignVertical: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    textAlignVertical: "center",
     height: 40,
     flex: 1,
     marginRight: 12,
@@ -357,13 +379,13 @@ const styles = StyleSheet.create({
   },
   popUpContainer: {
     flex: 1,
-    backgroundColor: 'rgba(52, 52, 52, 0.8)',
-    height: '100%',
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    alignSelf: 'center',
+    backgroundColor: "rgba(52, 52, 52, 0.8)",
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    alignSelf: "center",
     // paddingHorizontal: '25%',
     top: 0,
     left: 0,
@@ -371,65 +393,60 @@ const styles = StyleSheet.create({
     bottom: 0,
   },
   popUp: {
-    position: 'absolute',
-    backgroundColor: 'lightgray',
-    borderColor: 'black',
+    // position: 'absolute',
+    backgroundColor: "lightgray",
+    borderColor: "black",
     borderWidth: 0.6,
     borderRadius: 15,
-    width: 'auto',
-    height: 'auto',
-    bottom: 350,
+    // width: 'auto',
+    // height: 'auto',
+    // bottom: 350,
+    // flex: 1,
   },
   popUpTitle: {
     // backgroundColor: 'pink',
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     // top: -10,
   },
   popUpButtons: {
-    flexDirection: 'column',
+    flexDirection: "column",
     // justifyContent: 'center',
     // alignItems: 'center',
     right: 0,
     left: 0,
-    overflow: 'hidden',
+    overflow: "hidden",
     // backgroundColor: 'red',
   },
   popUpButton: {
     paddingVertical: 10,
     borderTopWidth: 0.6,
     // backgroundColor: 'pink',
-    width: '100%',
+    width: "100%",
   },
   popUpReserveButtons: {
-    flexDirection: 'row',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    right: 0,
-    left: 0,
-    overflow: 'hidden',
+    flexDirection: "row",
     borderTopWidth: 0.6,
     // backgroundColor: 'red',
+    // marginBottom: -16,
   },
   popUpReserveButton: {
     paddingVertical: 10,
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
     flex: 1,
   },
   reserveFee: {
     fontSize: 12,
-    fontWeight: '600',
-    color: 'black',
+    fontWeight: "600",
+    color: "black",
     borderWidth: 0.8,
-    borderColor: 'black',
+    borderColor: "black",
     borderRadius: 10,
-    paddingVertical: '2%',
-    paddingHorizontal: '3%',
-    marginTop: '3%',
+    paddingVertical: "2%",
+    paddingHorizontal: "3%",
+    marginTop: "3%",
     top: 10,
     width: 200,
   },
